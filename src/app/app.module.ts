0f457aeac021008c1dd3d41971249747f5f4b801
Rohan Bolusani <bolusanir@gmail.com>
2019-07-13 15:37:07 +0530

    databse connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import * as firebase from 'firebase';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD9lBFZ5g5V5q6ztEiOxLKXpADcKKXvbDg",
+  authDomain: "learning-curve-18a65.firebaseapp.com",
+  databaseURL: "https://learning-curve-18a65.firebaseio.com",
+  projectId: "learning-curve-18a65",
+  storageBucket: "",
+  messagingSenderId: "265200665737",
+  appId: "1:265200665737:web:a22bd80a55f1d566"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
